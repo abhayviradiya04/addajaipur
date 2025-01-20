7a82a2d64aa9e4333a622948dc821379654fa669
Abhay Viradiya <121176886+abhayviradiya04@users.noreply.github.com>
2025-01-20 13:13:40 +0530

    .

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import logo from './logo.svg';
+
 import './App.css';
 import Header from './Componet/Header/Header';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
 
 
 function App() {
