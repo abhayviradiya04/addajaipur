d6c6c948e7b1f145b9d07ddef9a94a7b5429d4ef
Abhii039 <abhidobariya2004@gmail.com>
2025-01-23 17:28:18 +0530

    User and Product schema and routes created.

--- Backend/models/Product.js
+++ Backend/models/Product.js
@@ -5,20 +5,16 @@ const productSchema = new Schema({
   name: { type: String, required: true },
   image: [{ type: String }], // Array of image URLs
   description: { type: String, required: true },
-  sku: { type: String, required: true },
   brand: {
     name: { type: String, required: true }
   },
-   
   price: { type: Number, required: true },
   material: { type: String, required: true },
   pattern: { type: String, required: true },
   type: { type: String, required: true },
   fabricCare: { type: String, required: true },
   lengthType: { type: String, required: true },
-  idealFor: { type: String, required: true },
   neck: { type: String, required: true },
-  sleeve: { type: String, required: true }
 }, { timestamps: true }); // Optional: Adds createdAt and updatedAt fields
 
 const Product = mongoose.model('Product', productSchema);
---
+++ Backend/models/User.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    wishlistItems: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Product', // Assuming you have a Product model
+    }],
+    cartItems: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Product', // Assuming you have a Product model
+    }],
+    orders: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Order', // Assuming you have an Order model
+    }],
+}, {
+    timestamps: true, // Automatically manage createdAt and updatedAt fields
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
